c04002caea5f7735ffca8a5734037249df3908dc
Ian Mateo Romero Sala <ianromerosala@gmail.com>
2026-07-02 17:38:07 -0300

    cambios

--- Carnelevarium - Progamacion 2 1er Parcial/Assets/Scripts/Interactables/Generator.cs
+++ Carnelevarium - Progamacion 2 1er Parcial/Assets/Scripts/Interactables/Generator.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -23,6 +23,31 @@ public class Generator : ItemCheck , IInteractable
     public bool hasUsedFuse;
     public bool hasUsedValve;
 
+    [Header("Referencias")]
+    public Animator crowbarAnimator;
+    public InteractionController interactionController;
+
+    [Header("Configuraci�n")]
+    public float animationDuration = 3f;
+
+    private bool hasBeenFixed = false;
+
+
+    [Header("Luces")]
+    public Light luzCrowbar;  // se pone verde al usar la crowbar
+    public Light luzFuse;     // se pone verde al usar el fusible
+    public Light luzValve;    // se pone verde al usar la v�lvula
+
+    public Color colorApagado = Color.red;
+    public Color colorEncendido = Color.green;
+
+    void Start()
+    {
+        luzCrowbar.color = colorApagado;
+        luzFuse.color = colorApagado;
+        luzValve.color = colorApagado;
+    }
+
     public void Update()
     {
         if (hasUsedValve == true)
@@ -46,20 +71,23 @@ public class Generator : ItemCheck , IInteractable
         {
             if (!hasUsedCrowbar)
             {
-                StartCoroutine(HasInteracted(crowbarDialogue, dialogueTime));
-
+                //StartCoroutine(HasInteracted(crowbarDialogue, dialogueTime));
+                StartCoroutine(CrowbarAnimation()); // arranca la animaci�n
                 hasUsedCrowbar = true;
                 hasFoundItem = false;
+
             }
             else if (hasUsedCrowbar && !hasUsedFuse)
             {
                 StartCoroutine(HasInteracted(fuseDialogue, dialogueTime));
                 hasUsedFuse = true;
                 hasFoundItem = false;
+                luzFuse.color = colorEncendido;    // segunda luz verde
             }
             else if (hasUsedCrowbar && hasUsedFuse && !hasUsedValve)
             {
                 hasUsedValve = true;
+                luzValve.color = colorEncendido;   // tercera luz verde
             }
         }
         else if (!hasFoundItem)
@@ -87,5 +115,28 @@ public class Generator : ItemCheck , IInteractable
         yield return new WaitForSeconds(time);
 
         UIManager.instance.dialogueText.text = "";
+    }
+
+    IEnumerator CrowbarAnimation()
+    {
+        // 1. Bloquear al jugador
+        interactionController.LockInteraction();
+
+        // 2. Mostrar di�logo
+        UIManager.instance.dialogueText.text = crowbarDialogue;
+
+        // 3. Reproducir animaci�n de la crowbar
+        crowbarAnimator.SetTrigger("Fix");
+
+
+        // 4. Esperar a que termine la animaci�n
+        yield return new WaitForSeconds(animationDuration);
+
+        // 5. Limpiar di�logo y desbloquear jugador
+        UIManager.instance.dialogueText.text = "";
+        interactionController.UnlockInteraction();
+
+        // 6. Cambiar el color de la luz a verde
+        luzCrowbar.color = colorEncendido;
     }
 }
